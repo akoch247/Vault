d255ed9dcf23af33b37633d2743ad5c36a35d7e1
akoch247 <159341179+akoch247@users.noreply.github.com>
2025-03-03 20:49:12 -0500

    Create vault.js

---
+++ vault.js
@@ -0,0 +1,11 @@
+// step 1: declare 3 variables//
+// step 2: write out the string//
+// step 3: create a dialogue box//
+
+//declaring the variables//
+let num1 = 10 * 1;
+let num2 = 80 / 2;
+let num3 = 40 - 1;
+
+//creating alert message//
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`);
